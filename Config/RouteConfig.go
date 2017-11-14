233799d21beb17740c7a0c866289108671977030
Deniz Gökçe <denizgokce93@gmail.com>
2017-11-14 23:49:56 +0200

    * Static HTML Routing Added * Welcome Page Added

--- Config/RouteConfig.go
+++ Config/RouteConfig.go
@@ -3,11 +3,15 @@ package Config
 import (
 	"github.com/gorilla/mux"
 	controller "ExampleRESTApi/Controllers"
+	"net/http"
 )
 
 func InitializeRoutes(router *mux.Router){
+	//Configuring Controller's Routes
 	router.HandleFunc("/people", controller.GetPeopleEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", controller.GetPersonEndpoint).Methods("GET")
 	router.HandleFunc("/people/{id}", controller.CreatePersonEndpoint).Methods("POST")
 	router.HandleFunc("/people/{id}", controller.DeletePersonEndpoint).Methods("DELETE")
+	//Configuring Static Page's Routes
+	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./Views/")))
 }
