9c6b9567b4c693fb45df6a32015a0a49e5dbd99a
Deniz Gökçe <denizgokce93@gmail.com>
2017-11-15 00:09:59 +0200

    * PORT variable added for Heroku

--- main.go
+++ main.go
@@ -7,19 +7,33 @@ import (
 	model "ExampleRESTApi/Models"
 	db "ExampleRESTApi/DataSource"
 	config "ExampleRESTApi/Config"
+	"os"
+	"fmt"
 )
 
+func determineListenAddress() (string, error) {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "", fmt.Errorf("$PORT not set")
+	}
+	return ":" + port, nil
+}
+
 func main() {
+
+	addr, err := determineListenAddress()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := mux.NewRouter()
 	config.InitializeRoutes(router) //Initializing The Routes Of Controllers
 
 	db.GetInstance().People = append(db.GetInstance().People, model.Person{ID: "1", Firstname: "Nic", Lastname: "Raboy", Address: &model.Address{City: "Dublin", State: "CA"}})
 	db.GetInstance().People = append(db.GetInstance().People, model.Person{ID: "2", Firstname: "Maria", Lastname: "Raboy"})
 
-	println("Server is running in port 5000...")
+	fmt.Printf("Server is running in port %s...", addr)
 
-	log.Fatal(http.ListenAndServe(":5000", router)) //Listening the HTTP Requests at Port 5000
-
-
+	log.Fatal(http.ListenAndServe(addr, router)) //Listening the HTTP Requests
 
 }
